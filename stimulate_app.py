3b51b11309c68c7ef780c7dee3b6e0db50a7a1ee
Anees-Amjad <147302700+Anees-Amjad@users.noreply.github.com>
2023-12-01 04:54:23 +0500

    Create stimulate_app.py

---
+++ stimulate_app.py
@@ -0,0 +1,29 @@
+# Tatooine Data (Planet 1)
+tatooine_data = [
+    # City 1
+    [100, 80, 5],
+    # City 2
+    [120, 90, 4],
+    #City 3
+    [110, 85, 6],
+    # City 4
+    [115, 88, 3],
+]
+
+# Naboo Data (Planet 2)
+naboo_data = [
+    [90, 70, 4], 
+    [110, 85, 3], 
+    [100, 80, 5], 
+    [95, 75, 2]
+]
+
+
+# Alderaan Data (Planet 3)
+alderaan_data = [
+    [85, 65, 3], 
+    [105, 80, 2], 
+    [95, 75, 4], 
+    [90, 70, 1]
+]
+
